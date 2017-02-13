faf36dfd9955fad61d9e546474a7c23dd61fe567
trentspi <tspice23@att.net>
2017-02-13 00:11:04 -0500

    Added for loop of characters and str var

--- variables.rs
+++ variables.rs
@@ -3,7 +3,7 @@ use std::mem;
 const LINESIZE:i8 = 1024;
 
 fn main() {
-  
+
   let a1 = -78912312;
   let a2:i16 = 2;
   let mut a3:u32 = 123;
@@ -11,4 +11,11 @@ fn main() {
 
   println!("The size of a1 is {} bytes", mem::size_of_val(&a1));
 
+  let ch= 'S';
+  let str = "BANANA";
+  println!("Taking BANANA and adding S to the end length of str times");
+  for x in str.chars() {
+    println!("For loop iteration character: {}",x);
+    println!("{}{}",str,ch);
+  }
 }
