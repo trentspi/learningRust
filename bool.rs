80f6d51e4851127d169067d41992600de21a0a6e
trentspi <tspice23@att.net>
2017-02-13 00:12:23 -0500

    Added booleans

---
+++ bool.rs
@@ -0,0 +1,7 @@
+fn main() {
+  let it_is_true = true;
+  let it_is_false = false;
+
+  println!("A true boolean returns: {}", it_is_true);
+  println!("A false boolean returns: {}", it_is_false);
+}
